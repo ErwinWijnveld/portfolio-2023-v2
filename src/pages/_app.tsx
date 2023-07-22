ce653ad7384ae86c9201f74e0bf4745bd22f309f
ErwinWijnveld <erwin.wijnveld@gmail.com>
2023-07-22 20:44:32 +0200

    feat: gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
 import localFont from 'next/font/local';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const poppins = Poppins({
 	subsets: ['devanagari'],
@@ -18,10 +20,25 @@ const monumentextended = localFont({
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className={poppins.variable + ' ' + monumentextended.variable}>
-			<PageTransitionGrid>
-				<Component {...pageProps} />
-			</PageTransitionGrid>
-		</div>
+		<>
+			<Head>
+				<Script
+					id="gtm"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N7KB8DVV');`,
+					}}
+				/>
+			</Head>
+			<div className={poppins.variable + ' ' + monumentextended.variable}>
+				<PageTransitionGrid>
+					<Component {...pageProps} />
+				</PageTransitionGrid>
+			</div>
+		</>
 	);
 }
