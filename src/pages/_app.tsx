322077d83701d8232065b48d757132e2471df1a0
ErwinWijnveld <erwin.wijnveld@gmail.com>
2023-07-22 20:51:27 +0200

    feat: final

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import PageTransitionGrid from '@/components/PageTransitionGrid';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import { Head } from 'next/document';
 import { Poppins } from 'next/font/google';
 import localFont from 'next/font/local';
 import Script from 'next/script';
@@ -20,17 +19,15 @@ const monumentextended = localFont({
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<Script id="gtm" strategy="afterInteractive">
-					{`
-						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-						})(window,document,'script','dataLayer','GTM-N7KB8DVV');
-					`}
-				</Script>
-			</Head>
+			<Script id="gtm" strategy="afterInteractive">
+				{`
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-N7KB8DVV');
+				`}
+			</Script>
 			<div className={poppins.variable + ' ' + monumentextended.variable}>
 				<PageTransitionGrid>
 					<Component {...pageProps} />
