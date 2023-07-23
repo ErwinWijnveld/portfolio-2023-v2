53a8a4848a418f58b73752371a9fe168e5386b04
ErwinWijnveld <erwin.wijnveld@gmail.com>
2023-07-23 13:18:08 +0200

    feat: favicon and sitemap www

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://erwinwijnveld.nl',
+    siteUrl: process.env.SITE_URL || 'https://www.erwinwijnveld.nl',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
